a9a261abdf57fe656feabe516496e5d0b9bab9fd
vermeer314 <vermeer314@gmail.com>
2025-12-11 23:59:56 +0900

    revise

--- search-filter-list/main.js
+++ search-filter-list/main.js
@@ -31,8 +31,12 @@ const words = [
 //2. data 배열을 돌면서 해당 문자열을 포함한 단어를 필터링하여 배열로 반환한다
 //3. 반환된 배열을 가지고 ul의 자식으로 li를 생성한다
 
+const ul = document.getElementById('ul');
+const input = document.getElementById('input');
+
 function stringFilter(arr, str) {
-  return arr.filter((word) => word.includes(str));
+  const keyword = str.toLowerCase(); //대소문자 구분 없이
+  return arr.filter((word) => word.includes(keyword));
 }
 
 function removeAllChild(parent) {
@@ -42,7 +46,6 @@ function removeAllChild(parent) {
 }
 
 function makeList(arr) {
-  const ul = document.getElementById('ul');
   removeAllChild(ul);
 
   arr.forEach((v) => {
@@ -53,19 +56,19 @@ function makeList(arr) {
 }
 
 function noResult() {
-  const ul = document.getElementById('ul');
   removeAllChild(ul);
 
   const li = document.createElement('li');
   li.textContent = 'No result';
+  li.style.listStyleType = 'none';
   ul.appendChild(li);
 }
 
-makeList(words); //초기화면 생성
-
 function searchFilterList() {
-  const inputValue = document.getElementById('input').value;
+  const inputValue = input.value;
 
   const filteredList = stringFilter(words, inputValue);
   filteredList.length != 0 ? makeList(filteredList) : noResult();
 }
+
+makeList(words); //초기 화면 생성
